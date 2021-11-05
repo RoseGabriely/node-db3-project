9850b86e56f3a6194597c7af0b45cf2631eb377e
RoseGabriely <rosegabriely14@gmail.com>
2021-11-05 08:28:54 -0400

    .

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -18,27 +18,33 @@ const checkSchemeId = async (req, res, next) => {
   }
 };
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
+const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    !scheme_name.trim() ||
+    typeof scheme_name !== "string"
+  ) {
+    next({ status: 400, message: "invalid scheme_name" });
+  } else {
+    next();
   }
-*/
-const validateScheme = (req, res, next) => {};
+};
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-  next();
+  const { instructions, step_number } = req.body;
+
+  if (
+    !instructions ||
+    !instructions.trim() ||
+    typeof instructions !== "string" ||
+    typeof step_number !== "number" ||
+    step_number < 1
+  ) {
+    next({ status: 400, message: "invalid step" });
+  } else {
+    next();
+  }
 };
 
 module.exports = {
