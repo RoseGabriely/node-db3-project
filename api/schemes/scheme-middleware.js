eeb9e0fa6a105ab16f2949707b72d01567e1fa22
RoseGabriely <rosegabriely14@gmail.com>
2021-11-05 06:16:26 -0400

    created id validating middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,13 +1,21 @@
-/*
-  If `scheme_id` does not exist in the database:
+const db = require("../../data/db-config");
 
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const existing = await db("schemes")
+      .where("scheme_id", req.params.scheme_id)
+      .first();
+    if (!existing) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
   }
-*/
-const checkSchemeId = (req, res, next) => {
-  next();
 };
 
 /*
@@ -18,9 +26,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-  next();
-};
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
